357e326d7b56f59fe49f3d8838e192f8a4e4a1e0
jomujin <jomujin@gmail.com>
2023-01-12 01:59:35 +0900

    config를 추가하라

---
+++ config/config.py
@@ -0,0 +1,27 @@
+from classes.DBConn import DBConn
+from condition.config_condition import (
+    AWS_AURORA_CONFIG
+)
+
+
+# postgres url variables from environment
+AWS_PG_HOST = AWS_AURORA_CONFIG.get('ENDPOINT', False)
+AWS_PG_PORT = AWS_AURORA_CONFIG.get('PORT', False)
+AWS_PG_DBNAME = AWS_AURORA_CONFIG.get('DBNAME', False)
+AWS_PG_USER = AWS_AURORA_CONFIG.get('USER', False)
+AWS_PG_REGION = AWS_AURORA_CONFIG.get('REGION', False)
+AWS_PG_PASSWORD = AWS_AURORA_CONFIG.get('PASSWORD', False)
+assert AWS_PG_HOST  is not False
+assert AWS_PG_USER is not False
+assert AWS_PG_PORT == '5432'
+assert AWS_PG_REGION is not False
+assert AWS_PG_DBNAME is not False
+assert AWS_PG_PASSWORD is not False
+
+TEST_AWS_DB = DBConn(
+    AWS_PG_HOST,
+    AWS_PG_PORT,
+    AWS_PG_USER,
+    AWS_PG_PASSWORD,
+    AWS_PG_DBNAME
+)

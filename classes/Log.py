6ddaa74f9407e1c3ef31dad1af5b58c99b1a2493
jomujin <jomujin@gmail.com>
2023-01-12 15:07:27 +0900

    Log class를 업데이트하라

--- classes/Log.py
+++ classes/Log.py
@@ -17,16 +17,38 @@ class Log:
         }
     
     def stream_handler(self, level: str):
-        """
-        level :
-        > "DEBUG" : logging.DEBUG , 
-        > "INFO" : logging.INFO , 
-        > "WARNING" : logging.WARNING , 
-        > "ERROR" : logging.ERROR , 
-        > "CRITICAL" : logging.CRITICAL , 
-        """
-        self.log.setLevel(self.levels[level])
-        streamHandler = logging.StreamHandler()
-        streamHandler.setFormatter(self.formatter)
-        self.log.addHandler(streamHandler)
-        return self.log
+        if len(self.log.handlers) > 0:
+            return self.log # Logger already exists
+        else:
+            """
+            level :
+            > "DEBUG" : logging.DEBUG , 
+            > "INFO" : logging.INFO , 
+            > "WARNING" : logging.WARNING , 
+            > "ERROR" : logging.ERROR , 
+            > "CRITICAL" : logging.CRITICAL , 
+            """
+            self.log.setLevel(self.levels[level])
+            streamHandler = logging.StreamHandler()
+            streamHandler.setFormatter(self.formatter)
+            self.log.addHandler(streamHandler)
+            return self.log
+
+def get_export_csv_log(
+    dnm: str,
+    pull_tnm: str,
+    s3nm: str,
+    destination_s3_file_path: str,
+    files_uploaded: int,
+    rows_uploaded: int,
+    bytes_uploaded: int
+):
+    return (f"""Success Export {dnm}{pull_tnm}.csv to AWS S3 {s3nm}{destination_s3_file_path} |
+files_uploaded: {files_uploaded} File(s) | rows_uploaded: {rows_uploaded} Row(s) | bytes_uploaded: {bytes_uploaded/(10**6)} Mb""")
+
+def get_export_ddl_log(
+    dnm: str,
+    pull_tnm: str,
+    destination_s3_file_path: str
+):
+    return (f"Success Export {dnm}{pull_tnm} DDL to AWS S3 {destination_s3_file_path}")

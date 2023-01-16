9dfff86c8ca5c86aa2de06a1a9a4d0538c2fa87e
jomujin <jomujin@gmail.com>
2023-01-16 13:15:51 +0900

    ToS3 업데이트하라

--- classes/ToS3.py
+++ classes/ToS3.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import botocore
 import typing
+import psycopg2
 from classes.Log import (
     Log,
     get_export_csv_log,
@@ -69,7 +70,7 @@ class ToS3:
             if e.response['Error']['Code'] == "404":
                 return False
             else:
-                raise
+                raise Exception
 
 
     def get_pnm(
@@ -118,79 +119,75 @@ class ToS3:
         database_name: str, # db name
         schema_name: str, # schema name
         table_name: str, # table name
-        if_exist: str = "fail"
+        if_exist: str = "replace"
     ):
         """
         database_name : database name
         schema_name : schema name
         table_name : table name
-        if_exists : {'fail', 'replace', 'append'}, default 'fail'
-            How to behave if the table already exists.
+        if_exist : {'replace', 'pass}, default 'replace'
+            How to behave if the object already exists in s3.
 
-            * fail: Raise a ValueError.
-            * replace: Drop the table before inserting new values.
-            * append: Insert new values to the existing table.
-
-        Raises
-        ------
-        ValueError
-            When the table already exists and `if_exists` is 'fail' (the
-            default).
+            * replace: Add new object to s3. It will be replaced or versioned according to the s3 bucket versioning settings.
+            * pass: Terminate without doing task.
 
         """
-        # if_exist_arguments = {
-        #     "fail" : ,
-        #     'pass' : ,
-        #     "append" : logging.INFO
-        # }
-
-        logger = Log("EXPORT_CSV_LOG").stream_handler("INFO")
-        table_name, partition_name = self.get_pnm(self, schema_name, table_name)
-
-        if partition_name:
-            pull_table_name = f'{schema_name}.{partition_name}'
-            export_table_sql_line = f'select * from {pull_table_name}'
-            dest_s3_file_path = f'{database_name}/{schema_name}/{table_name}/{partition_name}.csv'
-        else:
-            pull_table_name = f'{schema_name}.{table_name}'
-            export_table_sql_line = f'select * from {pull_table_name}'
-            dest_s3_file_path = f'{database_name}/{schema_name}/{table_name}/{table_name}.csv'
         
-        is_exist = self.check_exist_file(dest_s3_file_path)
-        print(is_exist)
-
-        sql = f"""
-        select * from aws_s3.query_export_to_s3(
-            '{export_table_sql_line}', 
-            aws_commons.create_s3_uri(
-            '{self.aws_s3_name}', 
-            '/{dest_s3_file_path}', 
-            '{self.aws_s3_region}'
-            ),
-            options :='format csv, HEADER true'
-        )
-        """
+        if_exist_args = ['replace', 'pass']
+        if if_exist not in if_exist_args:
+            raise Exception('Argument Error if_exist ')
 
         try:
-            self.cur.execute(sql)
-            res = self.cur.fetchall()
-            rows_uploaded, files_uploaded, bytes_uploaded = res[0]
-            # 출력 파라미터
-            # OUT rows_uploaded bigint: 지정된 쿼리에 대해 Amazon S3에 성공적으로 업로드된 테이블 행 수
-            # OUT files_uploaded bigint: Amazon S3에 업로드된 파일 수(파일은 약 6GB 크기로 생성, 생성된 각 추가 파일 이름에 _partXX가 추가됩니다. XX는 2, 3 등을 나타냄)
-            # OUT bytes_uploaded bigint: Amazon S3에 업로드된 총 바이트 수
-            msg = get_export_csv_log(
-                database_name = database_name,
-                pull_table_name = pull_table_name,
-                dest_s3_file_path = dest_s3_file_path,
-                files_uploaded = files_uploaded,
-                rows_uploaded = rows_uploaded,
-                bytes_uploaded = bytes_uploaded,
-            )
-            logger.info(msg)
+            logger = Log("EXPORT_CSV_LOG").stream_handler("INFO")
+            table_name, partition_name = self.get_pnm(self, schema_name, table_name)
+
+            if partition_name:
+                pull_table_name = f'{schema_name}.{partition_name}'
+                export_table_sql_line = f'select * from {pull_table_name}'
+                dest_s3_file_path = f'{database_name}/{schema_name}/{table_name}/{partition_name}.csv'
+            else:
+                pull_table_name = f'{schema_name}.{table_name}'
+                export_table_sql_line = f'select * from {pull_table_name}'
+                dest_s3_file_path = f'{database_name}/{schema_name}/{table_name}/{table_name}.csv'
+            
+            is_exist = self.check_exist_file(dest_s3_file_path)
+            
+            if is_exist and if_exist == 'replace':
+                sql = f"""
+                select * from aws_s3.query_export_to_s3(
+                    '{export_table_sql_line}', 
+                    aws_commons.create_s3_uri(
+                    '{self.aws_s3_name}', 
+                    '/{dest_s3_file_path}', 
+                    '{self.aws_s3_region}'
+                    ),
+                    options :='format csv, HEADER true'
+                )
+                """
+
+                self.cur.execute(sql)
+                res = self.cur.fetchall()
+                rows_uploaded, files_uploaded, bytes_uploaded = res[0]
+                # 출력 파라미터
+                # OUT rows_uploaded bigint: 지정된 쿼리에 대해 Amazon S3에 성공적으로 업로드된 테이블 행 수
+                # OUT files_uploaded bigint: Amazon S3에 업로드된 파일 수(파일은 약 6GB 크기로 생성, 생성된 각 추가 파일 이름에 _partXX가 추가됩니다. XX는 2, 3 등을 나타냄)
+                # OUT bytes_uploaded bigint: Amazon S3에 업로드된 총 바이트 수
+                msg = get_export_csv_log(
+                    database_name = database_name,
+                    pull_table_name = pull_table_name,
+                    dest_s3_file_path = dest_s3_file_path,
+                    files_uploaded = files_uploaded,
+                    rows_uploaded = rows_uploaded,
+                    bytes_uploaded = bytes_uploaded,
+                )
+                logger.info(msg)
+            else:
+                logger.info('Since there is already an object at that path, It terminates the task.')
+
         except:
             logger.error('Failed Export CSV Table')
-            raise ValueError
+            raise Exception
+
 
 
     def dump_ddl_to_s3(
@@ -198,79 +195,75 @@ class ToS3:
         database_name: str, # db name
         schema_name: str, # schema name
         table_name: str, # table name
-        if_exist: str = "fail"
+        if_exist: str = "replace"
     ):
         """
         database_name : database name
         schema_name : schema name
         table_name : table name
-        if_exists : {'fail', 'replace', 'append'}, default 'fail'
-            How to behave if the table already exists.
+        if_exists : {'replace', 'pass}, default 'replace'
+            How to behave if the object already exists in s3.
 
-            * fail: Raise a ValueError.
-            * replace: Drop the table before inserting new values.
-            * append: Insert new values to the existing table.
-
-        Raises
-        ------
-        ValueError
-            When the table already exists and `if_exists` is 'fail' (the
-            default).
+            * replace: Add new object to s3. It will be replaced or versioned according to the s3 bucket versioning settings.
+            * pass: Quit without doing work.
 
         """
-        # if_exist_arguments = {
-        #     "fail" : ,
-        #     'pass' : ,
-        #     "append" : logging.INFO
-        # }
 
-        logger = Log("EXPORT_DDL_LOG").stream_handler("INFO")
-        table_name, partition_name = self.get_pnm(
-            self, 
-            schema_name, 
-            table_name
-        )
-
-        if partition_name:
-            pull_table_name = f'{schema_name}.{partition_name}'
-            dest_s3_file_path = f's3://{self.aws_s3_name}/{database_name}/{schema_name}/{table_name}/{partition_name}_DDL.sql'
-        else:
-            pull_table_name = f'{schema_name}.{table_name}'
-            dest_s3_file_path = f's3://{self.aws_s3_name}/{database_name}/{schema_name}/{table_name}/{table_name}_DDL.sql'
-
-        is_exist = self.check_exist_file(dest_s3_file_path)
-        print(is_exist)
-
-        dump_success = 1
-        sql = f"""pg_dump \
-        -h {self.host} \
-        -p {self.port} \
-        -d {self.database} \
-        -U {self.user} \
-        -w \
-        -t {pull_table_name} \
-        --schema-only | \
-        aws s3 cp - {dest_s3_file_path}"""
+        if_exist_args = ['replace', 'pass']
+        if if_exist not in if_exist_args:
+            raise Exception('Argument Error if_exist ')
 
         try:
-            subprocess.run(
-                sql,
-                shell=True,
-                env={**os.environ, "PGPASSWORD": f"{self.password}"}
+            logger = Log("EXPORT_DDL_LOG").stream_handler("INFO")
+            table_name, partition_name = self.get_pnm(
+                self, 
+                schema_name, 
+                table_name
             )
-        except:
-            dump_success = 0
 
-        if dump_success:
-            msg = get_export_ddl_log(
-                database_name = database_name,
-                pull_table_name = pull_table_name,
-                dest_s3_file_path = dest_s3_file_path
-            )
-            logger.info(msg)
-        else:
+            if partition_name:
+                pull_table_name = f'{schema_name}.{partition_name}'
+                dest_s3_file_path = f's3://{self.aws_s3_name}/{database_name}/{schema_name}/{table_name}/{partition_name}_DDL.sql'
+            else:
+                pull_table_name = f'{schema_name}.{table_name}'
+                dest_s3_file_path = f's3://{self.aws_s3_name}/{database_name}/{schema_name}/{table_name}/{table_name}_DDL.sql'
+
+            dump_success = 1
+            is_exist = self.check_exist_file(dest_s3_file_path)
+            
+            if is_exist and if_exist == 'replace':
+                sql = f"""pg_dump \
+                -h {self.host} \
+                -p {self.port} \
+                -d {self.database} \
+                -U {self.user} \
+                -w \
+                -t {pull_table_name} \
+                --schema-only | \
+                aws s3 cp - {dest_s3_file_path}"""
+
+                try:
+                    subprocess.run(
+                        sql,
+                        shell=True,
+                        env={**os.environ, "PGPASSWORD": f"{self.password}"}
+                    )
+                except:
+                    dump_success = 0
+                    raise subprocess.SubprocessError
+
+                if dump_success:
+                    msg = get_export_ddl_log(
+                        database_name = database_name,
+                        pull_table_name = pull_table_name,
+                        dest_s3_file_path = dest_s3_file_path
+                    )
+                    logger.info(msg)
+            else:
+                logger.info('Since there is already an object at that path, It terminates the task.')
+        except:
             logger.error('Failed Export DDL Table')
-            raise ValueError
+            raise Exception
 
 
     def get_all_multi_part_list(
@@ -301,7 +294,7 @@ class ToS3:
         if len(multi_part_list):
             return multi_part_list
         else:
-            raise ValueError
+            raise ValueError("None Values From list, Make suere the file_path argument is correct")
 
 
     def import_table_from_s3(
@@ -316,9 +309,13 @@ class ToS3:
         """
         import_schema_name : A required text string containing the name of the PostgreSQL database schema to import the data into
         import_table_name : A required text string containing the name of the PostgreSQL database table to import the data into
-        file_path : 
-        format : 
-        header :
+        file_path : The Amazon S3 file name including the path of the file.
+        format : {'csv', 'text'}, The default is 'csv'
+            * Selects the data format to be read or written: text, csv (Comma Separated Values), or binary.
+            * source : https://www.postgresql.org/docs/current/sql-copy.html
+        header : {'true', 'false'}, The default is 'csv'
+            * Specifies whether the selected option should be turned on or off. You can write 'true', and 'false'.
+            * source : https://www.postgresql.org/docs/current/sql-copy.html
         """
 
         multi_part_list = self.get_all_multi_part_list(file_path)
@@ -326,34 +323,34 @@ class ToS3:
             
             logger = Log("EXPORT_CSV_LOG").stream_handler("INFO")
             
-            for idx, multi_part_path in enumerate(multi_part_list):
-                if idx < 3:
-                    sql = f"""
-                    SELECT aws_s3.table_import_from_s3(
-                        '{import_schema_name}.{import_table_name}',
-                        '', 
-                        '(format {format}, HEADER {header})',
-                        aws_commons.create_s3_uri(
-                            '{self.aws_s3_name}',
-                            '/{multi_part_path}',
-                            '{self.aws_s3_region}'
-                        )
+            for multi_part_path in multi_part_list:
+                sql = f"""
+                SELECT aws_s3.table_import_from_s3(
+                    '{import_schema_name}.{import_table_name}',
+                    '', 
+                    '(format {format}, HEADER {header})',
+                    aws_commons.create_s3_uri(
+                        '{self.aws_s3_name}',
+                        '/{multi_part_path}',
+                        '{self.aws_s3_region}'
                     )
-                    """
+                )
+                """
 
-                    try:
-                        self.cur.execute(sql)
-                        res = self.cur.fetchall()
-                        if res:
-                            msg = get_import_table_log(
-                                database_name = import_database_name,
-                                pull_table_name = f'{import_schema_name}.{import_table_name}',
-                                source_s3_file_path = f's3://{self.aws_s3_name}/{multi_part_path}',
-                                result_msg = res[0][0]
-                            )
-                            logger.info(msg)
-                    except:
+                try:
+                    self.cur.execute(sql)
+                    res = self.cur.fetchall()
+                    if res:
+                        msg = get_import_table_log(
+                            database_name = import_database_name,
+                            pull_table_name = f'{import_schema_name}.{import_table_name}',
+                            source_s3_file_path = f's3://{self.aws_s3_name}/{multi_part_path}',
+                            result_msg = res[0][0]
+                        )
+                        logger.info(msg)
+                    else:
                         logger.error('Failed Import Table')
-                        raise ValueError
-                else:
-                    break
+                        raise psycopg2.errors.NoData
+                except :
+                    logger.error('Failed Import Table')
+                    raise Exception

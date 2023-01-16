3e3cdab06d19b1e7638f4e28a74de3ccd1dfc8d8
jomujin <jomujin@gmail.com>
2023-01-16 09:20:01 +0900

    ToS3 class를 업데이트하라

--- classes/ToS3.py
+++ classes/ToS3.py
@@ -1,12 +1,18 @@
 import os
 import subprocess
+import botocore
+import typing
 from classes.Log import (
     Log,
     get_export_csv_log,
-    get_export_ddl_log
+    get_export_ddl_log,
+    get_import_table_log
 )
-from condition.config_cond import (
-    AWS_AURORA_CONFIG
+from classes.DBConn import (
+    DBConn
+)
+from classes.Client import (
+    Boto3Client
 )
 
 
@@ -14,71 +20,159 @@ class ToS3:
 
     def __init__(
         self, 
-        con, # db Connection
-        dnm: str, # db name
-        snm: str, # schema name
-        tnm: str, # table name
-        s3nm: str, # destination s3 bucket name
-        s3region: str # destination s3 bucket region
+        host: str,
+        port: str,
+        user: str,
+        password: str,
+        database: str,
+        aws_access_key_id: str, # aws access key id
+        aws_secret_access_key: str, # aws secret access key
+        aws_s3_name: str, # s3 bucket name
+        aws_s3_region: str # s3 bucket region
     ):
-        self.con = con
-        self.cur = con.cursor()
-        self.dnm = dnm
-        self.snm = snm
-        self.tnm = tnm
-        self.pnm = None
-        self.s3nm = s3nm
-        self.s3region = s3region
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.aws_s3_name = aws_s3_name
+        self.aws_s3_region = aws_s3_region
+        self.con = DBConn(
+            host,
+            port,
+            user,
+            password,
+            database
+        ).getPsypgConn()
+        self.cur = self.con.cursor()
+        self.session = Boto3Client(
+            self.aws_access_key_id,
+            self.aws_secret_access_key,
+            self.aws_s3_name,
+            self.aws_s3_region
+        )
 
-    def get_pnm(self):
-        get_pnm_sql = f"""
+
+    def check_exist_file(
+        self,
+        file_path: str
+    ) -> bool:
+        try:
+            self.session.s3_client.head_object(
+                Bucket=self.aws_s3_name,
+                Key=file_path
+            )
+            return True
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                return False
+            else:
+                raise
+
+
+    def get_pnm(
+        self,
+        schema_name: str,
+        table_name: str
+    ) -> typing.Tuple[str, str or None]:
+        """
+        schema_name : schema name
+        table_name : table name
+        """
+
+        sql = f"""
         select 
             inhrelid::regclass,
             inhparent::regclass 
         from pg_inherits 
         where inhrelid IN (
             select oid from pg_class 
-            WHERE relname = '{self.tnm}' 
-            AND relnamespace = (SELECT oid FROM pg_namespace 
-            WHERE nspname = '{self.snm}'))
+            WHERE relname = '{table_name}' 
+            AND relnamespace = (
+                SELECT 
+                    oid 
+                FROM pg_namespace 
+                WHERE nspname = '{schema_name}'
+            )
+        )
         """
         try:
-            self.cur.execute(get_pnm_sql)
+            self.cur.execute(sql)
             res = self.cur.fetchall()
             if res:
-                pnm, tnm = res[0]
-                self.tnm: str = tnm.replace(f'{self.snm}.', '')
-                self.pnm: str = pnm.replace(f'{self.snm}.', '')
+                partition_name, table_name = res[0]
+                table_name: str = table_name.replace(f'{schema_name}.', '')
+                partition_name: str = partition_name.replace(f'{schema_name}.', '')
+                return table_name, partition_name
+            else:
+                return table_name, None
         except:
-            pass
+            return table_name, None
             # print('Not Partitioning Table')
 
-    def export_csv_to_s3(self):
-        logger = Log("EXPORT_CSV_LOG").stream_handler("INFO")
 
-        if self.pnm:
-            pull_tnm = f'{self.snm}.{self.pnm}'
-            export_table_sql_line = f'select * from {pull_tnm}'
-            destination_s3_file_path = f'/{self.dnm}/{self.snm}/{self.tnm}/{self.pnm}.csv'
+    def export_csv_to_s3(
+        self, 
+        database_name: str, # db name
+        schema_name: str, # schema name
+        table_name: str, # table name
+        if_exist: str = "fail"
+    ):
+        """
+        database_name : database name
+        schema_name : schema name
+        table_name : table name
+        if_exists : {'fail', 'replace', 'append'}, default 'fail'
+            How to behave if the table already exists.
+
+            * fail: Raise a ValueError.
+            * replace: Drop the table before inserting new values.
+            * append: Insert new values to the existing table.
+
+        Raises
+        ------
+        ValueError
+            When the table already exists and `if_exists` is 'fail' (the
+            default).
+
+        """
+        # if_exist_arguments = {
+        #     "fail" : ,
+        #     'pass' : ,
+        #     "append" : logging.INFO
+        # }
+
+        logger = Log("EXPORT_CSV_LOG").stream_handler("INFO")
+        table_name, partition_name = self.get_pnm(self, schema_name, table_name)
+
+        if partition_name:
+            pull_table_name = f'{schema_name}.{partition_name}'
+            export_table_sql_line = f'select * from {pull_table_name}'
+            dest_s3_file_path = f'{database_name}/{schema_name}/{table_name}/{partition_name}.csv'
         else:
-            pull_tnm = f'{self.snm}.{self.tnm}'
-            export_table_sql_line = f'select * from {pull_tnm}'
-            destination_s3_file_path = f'/{self.dnm}/{self.snm}/{self.tnm}/{self.tnm}.csv'
+            pull_table_name = f'{schema_name}.{table_name}'
+            export_table_sql_line = f'select * from {pull_table_name}'
+            dest_s3_file_path = f'{database_name}/{schema_name}/{table_name}/{table_name}.csv'
         
-        export_sql = f"""
+        is_exist = self.check_exist_file(dest_s3_file_path)
+        print(is_exist)
+
+        sql = f"""
         select * from aws_s3.query_export_to_s3(
             '{export_table_sql_line}', 
             aws_commons.create_s3_uri(
-            '{self.s3nm}', 
-            '{destination_s3_file_path}', 
-            '{self.s3region}'
+            '{self.aws_s3_name}', 
+            '/{dest_s3_file_path}', 
+            '{self.aws_s3_region}'
             ),
             options :='format csv, HEADER true'
         )
         """
 
         try:
-            self.cur.execute(export_sql)
+            self.cur.execute(sql)
             res = self.cur.fetchall()
             rows_uploaded, files_uploaded, bytes_uploaded = res[0]
             # 출력 파라미터
@@ -86,10 +180,9 @@ class ToS3:
             # OUT files_uploaded bigint: Amazon S3에 업로드된 파일 수(파일은 약 6GB 크기로 생성, 생성된 각 추가 파일 이름에 _partXX가 추가됩니다. XX는 2, 3 등을 나타냄)
             # OUT bytes_uploaded bigint: Amazon S3에 업로드된 총 바이트 수
             msg = get_export_csv_log(
-                dnm = self.dnm,
-                pull_tnm = pull_tnm,
-                s3nm = self.s3nm,
-                destination_s3_file_path = destination_s3_file_path,
+                database_name = database_name,
+                pull_table_name = pull_table_name,
+                dest_s3_file_path = dest_s3_file_path,
                 files_uploaded = files_uploaded,
                 rows_uploaded = rows_uploaded,
                 bytes_uploaded = bytes_uploaded,
@@ -99,43 +192,166 @@ class ToS3:
             logger.error('Failed Export CSV Table')
             raise ValueError
 
-    def dump_ddl_to_s3(self):
-        logger = Log("EXPORT_DDL_LOG").stream_handler("INFO")
 
-        if self.pnm:
-            pull_tnm = f'{self.snm}.{self.pnm}'
-            destination_s3_file_path = f's3://{self.s3nm}/{self.dnm}/{self.snm}/{self.tnm}/{self.pnm}_DDL.sql'
+    def dump_ddl_to_s3(
+        self,
+        database_name: str, # db name
+        schema_name: str, # schema name
+        table_name: str, # table name
+        if_exist: str = "fail"
+    ):
+        """
+        database_name : database name
+        schema_name : schema name
+        table_name : table name
+        if_exists : {'fail', 'replace', 'append'}, default 'fail'
+            How to behave if the table already exists.
+
+            * fail: Raise a ValueError.
+            * replace: Drop the table before inserting new values.
+            * append: Insert new values to the existing table.
+
+        Raises
+        ------
+        ValueError
+            When the table already exists and `if_exists` is 'fail' (the
+            default).
+
+        """
+        # if_exist_arguments = {
+        #     "fail" : ,
+        #     'pass' : ,
+        #     "append" : logging.INFO
+        # }
+
+        logger = Log("EXPORT_DDL_LOG").stream_handler("INFO")
+        table_name, partition_name = self.get_pnm(
+            self, 
+            schema_name, 
+            table_name
+        )
+
+        if partition_name:
+            pull_table_name = f'{schema_name}.{partition_name}'
+            dest_s3_file_path = f's3://{self.aws_s3_name}/{database_name}/{schema_name}/{table_name}/{partition_name}_DDL.sql'
         else:
-            pull_tnm = f'{self.snm}.{self.tnm}'
-            destination_s3_file_path = f's3://{self.s3nm}/{self.dnm}/{self.snm}/{self.tnm}/{self.tnm}_DDL.sql'
+            pull_table_name = f'{schema_name}.{table_name}'
+            dest_s3_file_path = f's3://{self.aws_s3_name}/{database_name}/{schema_name}/{table_name}/{table_name}_DDL.sql'
+
+        is_exist = self.check_exist_file(dest_s3_file_path)
+        print(is_exist)
 
         dump_success = 1
         sql = f"""pg_dump \
-        -h {AWS_AURORA_CONFIG.get('ENDPOINT')} \
-        -p {AWS_AURORA_CONFIG.get('PORT')} \
-        -d {AWS_AURORA_CONFIG.get('DBNAME')} \
-        -U {AWS_AURORA_CONFIG.get('USER')} \
+        -h {self.host} \
+        -p {self.port} \
+        -d {self.database} \
+        -U {self.user} \
         -w \
-        -t {pull_tnm} \
+        -t {pull_table_name} \
         --schema-only | \
-        aws s3 cp - {destination_s3_file_path}"""
+        aws s3 cp - {dest_s3_file_path}"""
 
         try:
             subprocess.run(
                 sql,
                 shell=True,
-                env={**os.environ, "PGPASSWORD": f"{AWS_AURORA_CONFIG.get('PASSWORD')}"}
+                env={**os.environ, "PGPASSWORD": f"{self.password}"}
             )
         except:
             dump_success = 0
 
         if dump_success:
             msg = get_export_ddl_log(
-                dnm = self.dnm,
-                pull_tnm = pull_tnm,
-                destination_s3_file_path = destination_s3_file_path
+                database_name = database_name,
+                pull_table_name = pull_table_name,
+                dest_s3_file_path = dest_s3_file_path
             )
             logger.info(msg)
         else:
             logger.error('Failed Export DDL Table')
             raise ValueError
+
+
+    def get_all_multi_part_list(
+        self,
+        file_path: str
+    ) -> typing.List[str]:
+        num = 1
+        multi_part_list = list()
+
+        while True:
+            if num < 2:
+                key_path = file_path
+            else:
+                key_path = file_path.replace('.csv', f'.csv_part{num}')
+
+            try:
+                self.session.s3_client.head_object(
+                    Bucket=self.aws_s3_name,
+                    Key=key_path
+                )
+                multi_part_list.append(key_path)
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == "404":
+                    break
+
+            num += 1
+
+        if len(multi_part_list):
+            return multi_part_list
+        else:
+            raise ValueError
+
+
+    def import_table_from_s3(
+        self,
+        import_database_name: str,
+        import_schema_name: str,
+        import_table_name: str,
+        file_path: str,
+        format: str = 'csv',
+        header: str = 'true',
+    ):
+        """
+        import_schema_name : A required text string containing the name of the PostgreSQL database schema to import the data into
+        import_table_name : A required text string containing the name of the PostgreSQL database table to import the data into
+        file_path : 
+        format : 
+        header :
+        """
+
+        multi_part_list = self.get_all_multi_part_list(file_path)
+        if multi_part_list:
+            
+            logger = Log("EXPORT_CSV_LOG").stream_handler("INFO")
+            
+            for multi_part_path in multi_part_list:
+                sql = f"""
+                SELECT aws_s3.table_import_from_s3(
+                    '{import_schema_name}.{import_table_name}',
+                    '', 
+                    '(format {format}, HEADER {header})',
+                    aws_commons.create_s3_uri(
+                        '{self.aws_s3_name}',
+                        '/{multi_part_path}',
+                        '{self.aws_s3_region}'
+                    )
+                )
+                """
+
+                try:
+                    self.cur.execute(sql)
+                    res = self.cur.fetchall()
+                    self.con.commit()
+                    if res:
+                        msg = get_import_table_log(
+                            database_name = import_database_name,
+                            pull_table_name = f'{import_schema_name}.{import_table_name}',
+                            source_s3_file_path = f's3://{self.aws_s3_name}/{multi_part_path}',
+                            result_msg = res[0]
+                        )
+                        logger.info(msg)
+                except:
+                    logger.error('Failed Import Table')
+                    raise ValueError
